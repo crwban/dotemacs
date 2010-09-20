c397095d1544889182cd0e7a9b9e2fdd71ad6e3e
Aled Griffiths <aled.griffiths@gmail.com>
2010-09-20 14:54:38 +0100

    Hide menu bar

--- init/ui.el
+++ init/ui.el
@@ -5,7 +5,7 @@
 
 ;; Hide toolbar and menu - learn the keyboard shortcuts instead!
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tooltip-mode) (tooltip-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
