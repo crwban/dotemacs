c52a060f7fc8fba706344fa834674d3f161afe8d
Aled Griffiths <aled.griffiths@gmail.com>
2010-09-20 16:37:57 +0100

    Enabled js2-mode

--- init/lang.el
+++ init/lang.el
@@ -104,7 +104,7 @@
 ;; JAVASCRIPT ****************************************************************
 
 ;; js2-mode (javascript)
-;(autoload 'js2-mode "js2" nil t)
-;(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 ;; END JAVASCRIPT ************************************************************
