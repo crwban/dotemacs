85f1159244ad0fcc77cb3b9d56f26afd91e86850
Aled Griffiths <aled.griffiths@gmail.com>
2012-03-04 16:19:57 +0000

    Fix bindings for windmove in org-mode buffers.

--- init/org.el
+++ init/org.el
@@ -9,6 +9,12 @@
 ;(setq org-CUA-compatible t)
 (setq org-replace-disputed-keys t)
 
+;; Make windmove work in org-mode:
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 ;(add-hook 'org-mode-hook 'flyspell-mode)
 
