8c9f6a773f6ca492845fd813da9ce12925a34e2f
Aled Griffiths <aled.griffiths@gmail.com>
2020-06-18 06:47:48 +0100

    Disabling tramp as it seemd to trigger some warnings.

--- init/linux.el
+++ init/linux.el
@@ -1,13 +1,6 @@
 ;; On Linux, hide the menu bar...
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
-;; tramp
-(add-to-list 'load-path "~/.emacs.d/site-lisp/tramp/lisp/")
-(require 'tramp)
-(setq tramp-default-method "sshc")
-; respect the PATH variable set
-(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
-
 ;; (add-to-list 'default-frame-alist '(font . "Dejavu Sans Mono-10"))
 (add-to-list 'default-frame-alist '(font . "Ubuntu Mono-11"))
 ;(add-to-list 'default-frame-alist '(font . "Inconsolata-10"))
